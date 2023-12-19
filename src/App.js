3f6366e4aa14e2e4cd55d04d2922ed100eb64336
prabhakar <prabhakarkumar16181@gmail.com>
2023-12-18 19:59:42 -0800

    E-commerce website creating start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>E-commerce website</h1>
+        <p>It's a very big project</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
